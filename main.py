d9c70c7ed2525155f0f3fd3b4d972ff420e99e34
Inozpavel <Inozpavel@mail.ru>
2020-09-16 23:00:52 +0300

    Изменено создание бота

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Bot import Bot
 
 
 def main():
-    bot = Bot(open("vk_key.txt").readline(), open("weather_key.txt", "r").readline())
+    Bot(open("vk_key.txt").readline(), open("weather_key.txt", "r").readline())
 
 
 if __name__ == '__main__':
